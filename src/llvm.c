0b1ede6df77f555b556593c685ceade039cd61b1
benmandrew <benmandrew@gmail.com>
2025-10-11 22:07:20 +0100

    Starting on branching, added exit label to main function

--- src/llvm.c
+++ src/llvm.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,14 +10,87 @@ struct llvm_function {
         LLVMTypeRef type;
 };
 
+#define JUMP_STACK_MAX_SIZE (128)
+
+struct llvm_jump_stack {
+        LLVMBasicBlockRef stack[JUMP_STACK_MAX_SIZE];
+        size_t head;
+};
+
+static struct llvm_jump_stack jump_stack_new() {
+        return (struct llvm_jump_stack){
+            .head = 0,
+        };
+}
+
+static void jump_stack_push(struct llvm_jump_stack *js, LLVMBasicBlockRef c) {
+        assert(js->head < JUMP_STACK_MAX_SIZE - 1);
+        js->stack[js->head] = c;
+        js->head++;
+}
+
+static LLVMBasicBlockRef jump_stack_pop(struct llvm_jump_stack *js) {
+        assert(js->head > 0);
+        js->head--;
+        return js->stack[js->head];
+}
+
 struct llvm_context {
         LLVMModuleRef module;
         LLVMBuilderRef builder;
         LLVMValueRef dp;
         LLVMValueRef data;
+        struct llvm_jump_stack js;
         struct llvm_function putchar;
         struct llvm_function getchar;
 };
+
+void create_putchar_declaration(struct llvm_context *ctx) {
+        ctx->putchar.type = LLVMFunctionType(
+            LLVMInt32Type(), (LLVMTypeRef[]){LLVMInt32Type()}, 1, 0);
+        ctx->putchar.func =
+            LLVMAddFunction(ctx->module, "putchar", ctx->putchar.type);
+}
+
+void create_getchar_declaration(struct llvm_context *ctx) {
+        ctx->getchar.type = LLVMFunctionType(LLVMInt32Type(), NULL, 0, 0);
+        ctx->getchar.func =
+            LLVMAddFunction(ctx->module, "getchar", ctx->getchar.type);
+}
+
+struct llvm_context create_module_preamble(const char *name) {
+        struct llvm_context ctx;
+        ctx.module = LLVMModuleCreateWithName(name);
+        ctx.builder = LLVMCreateBuilder();
+        create_putchar_declaration(&ctx);
+        create_getchar_declaration(&ctx);
+        ctx.dp = LLVMAddGlobal(ctx.module, LLVMInt32Type(), "dp");
+        LLVMSetInitializer(ctx.dp, LLVMConstNull(LLVMInt32Type()));
+        ctx.data = LLVMAddGlobal(
+            ctx.module, LLVMArrayType(LLVMInt8Type(), DATA_SIZE), "data");
+        LLVMSetInitializer(
+            ctx.data, LLVMConstNull(LLVMArrayType(LLVMInt8Type(), DATA_SIZE)));
+        ctx.js = jump_stack_new();
+        return ctx;
+}
+
+void dispose_module(LLVMModuleRef module) { LLVMDisposeModule(module); }
+
+void create_main_function(struct llvm_context *ctx) {
+        LLVMTypeRef main_type = LLVMFunctionType(LLVMInt32Type(), NULL, 0, 0);
+        LLVMValueRef main = LLVMAddFunction(ctx->module, "main", main_type);
+        LLVMBasicBlockRef entry_block = LLVMAppendBasicBlock(main, "entry");
+        LLVMPositionBuilderAtEnd(ctx->builder, entry_block);
+        LLVMBasicBlockRef exit_block = LLVMAppendBasicBlock(main, "exit");
+        jump_stack_push(&ctx->js, exit_block);
+}
+
+void finalise_main_function(struct llvm_context *ctx) {
+        LLVMBasicBlockRef exit_block = jump_stack_pop(&ctx->js);
+        LLVMBuildBr(ctx->builder, exit_block);
+        LLVMPositionBuilderAtEnd(ctx->builder, exit_block);
+        LLVMBuildRet(ctx->builder, LLVMConstInt(LLVMInt32Type(), 0, 0));
+}
 
 LLVMValueRef index_data(struct llvm_context *ctx) {
         LLVMValueRef dp_value =
@@ -87,51 +161,20 @@ void comma(struct llvm_context *ctx) {
         LLVMBuildStore(ctx->builder, char_value, data_ptr);
 }
 
-void create_putchar_declaration(struct llvm_context *ctx) {
-        ctx->putchar.type = LLVMFunctionType(
-            LLVMInt32Type(), (LLVMTypeRef[]){LLVMInt32Type()}, 1, 0);
-        ctx->putchar.func =
-            LLVMAddFunction(ctx->module, "putchar", ctx->putchar.type);
-}
-
-void create_getchar_declaration(struct llvm_context *ctx) {
-        ctx->getchar.type = LLVMFunctionType(LLVMInt32Type(), NULL, 0, 0);
-        ctx->getchar.func =
-            LLVMAddFunction(ctx->module, "getchar", ctx->getchar.type);
-}
-
-struct llvm_context create_module_preamble(const char *name) {
-        struct llvm_context ctx;
-        ctx.module = LLVMModuleCreateWithName(name);
-        create_putchar_declaration(&ctx);
-        create_getchar_declaration(&ctx);
-        ctx.dp = LLVMAddGlobal(ctx.module, LLVMInt32Type(), "dp");
-        LLVMSetInitializer(ctx.dp, LLVMConstNull(LLVMInt32Type()));
-        ctx.data = LLVMAddGlobal(
-            ctx.module, LLVMArrayType(LLVMInt8Type(), DATA_SIZE), "data");
-        LLVMSetInitializer(
-            ctx.data, LLVMConstNull(LLVMArrayType(LLVMInt8Type(), DATA_SIZE)));
-        return ctx;
-}
-
-void dispose_module(LLVMModuleRef module) { LLVMDisposeModule(module); }
-
-LLVMBuilderRef add_basic_block(LLVMValueRef func, const char *block_name) {
-        LLVMBasicBlockRef block = LLVMAppendBasicBlock(func, block_name);
-        LLVMBuilderRef builder = LLVMCreateBuilder();
-        LLVMPositionBuilderAtEnd(builder, block);
-        return builder;
-}
-
-LLVMValueRef add_main_function(LLVMModuleRef mod) {
-        LLVMTypeRef func_type = LLVMFunctionType(LLVMInt32Type(), NULL, 0, 0);
-        return LLVMAddFunction(mod, "main", func_type);
+void left_bracket(struct llvm_context *ctx, LLVMBasicBlockRef loop_body,
+                  LLVMBasicBlockRef after_loop) {
+        LLVMValueRef data_ptr = index_data(ctx);
+        LLVMValueRef current_value = LLVMBuildLoad2(
+            ctx->builder, LLVMInt8Type(), data_ptr, "current_val");
+        LLVMValueRef condition =
+            LLVMBuildICmp(ctx->builder, LLVMIntNE, current_value,
+                          LLVMConstInt(LLVMInt8Type(), 0, 0), "loopcond");
+        LLVMBuildCondBr(ctx->builder, condition, loop_body, after_loop);
 }
 
 LLVMModuleRef generate(struct program *p) {
         struct llvm_context ctx = create_module_preamble("main");
-        LLVMValueRef main = add_main_function(ctx.module);
-        ctx.builder = add_basic_block(main, "entry");
+        create_main_function(&ctx);
         for (size_t i = 0; i < p->length; i++) {
                 struct cmd c = p->cmds[i];
                 switch (c.type) {
@@ -162,7 +205,7 @@ LLVMModuleRef generate(struct program *p) {
                         exit(1);
                 }
         }
-        LLVMBuildRet(ctx.builder, LLVMConstInt(LLVMInt32Type(), 0, 0));
+        finalise_main_function(&ctx);
         LLVMDisposeBuilder(ctx.builder);
         return ctx.module;
 }
