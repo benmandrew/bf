9d30c50b334865c0957568bb6a38da429d5480e8
benmandrew <benmandrew@gmail.com>
2025-12-12 00:17:45 +0000

    Add cache to Go server

--- go/server.go
+++ go/server.go
@@ -9,6 +9,10 @@ import (
 	"os/exec"
 )
 
+var cache map[string][]byte
+var n_requests int64
+var n_cache_hits int64
+
 func runHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
@@ -26,6 +30,14 @@ func runHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed reading input", http.StatusBadRequest)
 		return
 	}
+	n_requests++
+	w.Header().Set("Content-Type", "text/plain")
+	if output, found := cache[string(input)]; found {
+		n_cache_hits++
+		log.Printf("Cache hit, total requests: %d, cache hits: %d", n_requests, n_cache_hits+1)
+		w.Write(output)
+		return
+	}
 	programPath := os.Args[1]
 	cmd := exec.Command(programPath)
 	cmd.Stdin = bytes.NewReader(input)
@@ -35,14 +47,18 @@ func runHandler(w http.ResponseWriter, r *http.Request) {
 	cmd.Stderr = &stderr
 	err = cmd.Run()
 	if err != nil {
-		http.Error(w, "bfc error:\n"+stderr.String(), http.StatusInternalServerError)
+		log.Printf("Error: %v, stderr: %s", err, stderr.String())
+		http.Error(w, stderr.String(), http.StatusBadRequest)
 		return
 	}
-	w.Header().Set("Content-Type", "text/plain")
 	w.Write(stdout.Bytes())
+	cache[string(input)] = stdout.Bytes()
 }
 
 func main() {
+	cache = make(map[string][]byte)
+	n_requests = 0
+	n_cache_hits = 0
 	http.HandleFunc("/compile", runHandler)
 	log.Println("Listening on :8000")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
