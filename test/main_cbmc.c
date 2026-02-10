1c7d727605ea4cb87712ee4791559452b7115369
benmandrew <benmandrew@gmail.com>
2026-02-10 22:39:22 +0000

    feat(test): Add model-checking script
    And move all scripts to dedicated directory.

--- test/main_cbmc.c
+++ test/main_cbmc.c
@@ -6,28 +6,29 @@
 #include "read.h"
 
 // Maximum length for the test string
-#define MAX_LEN 8
+#ifndef MAX_PROGRAM_LEN
+#define MAX_PROGRAM_LEN 4
+#endif
 
 int main() {
         // Allocate a buffer for the program string
-        char program[MAX_LEN + 1];
-        __CPROVER_assume(__CPROVER_r_ok(program, MAX_LEN + 1));
-        for (size_t i = 0; i < MAX_LEN; ++i) {
+        char program[MAX_PROGRAM_LEN + 1];
+        __CPROVER_assume(__CPROVER_r_ok(program, MAX_PROGRAM_LEN + 1));
+        for (size_t i = 0; i < MAX_PROGRAM_LEN; ++i) {
                 unsigned char c = nondet_uchar();
-                // __CPROVER_assume(c == '+' || c == '-' || c == '>' || c == '<'
-                // ||
-                //                  c == '.' || c == ',' || c == '[' || c == ']'
-                //                  || c == '\n' || c == '\r' || c == '\t' || c
-                //                  == '\v' || c == '\f' || c == ' ' || c ==
-                //                  '\0');
+                __CPROVER_assume(c == '+' || c == '-' || c == '>' || c == '<' ||
+                                 c == '.' || c == ',' || c == '[' || c == ']' ||
+                                 c == '\n' || c == '\r' || c == '\t' ||
+                                 c == '\v' || c == '\f' || c == ' ' ||
+                                 c == '\0');
                 program[i] = c;
                 if (c == '\0') {
-                        for (size_t j = i + 1; j < MAX_LEN + 1; ++j)
+                        for (size_t j = i + 1; j < MAX_PROGRAM_LEN + 1; ++j)
                                 program[j] = '\0';
                         break;
                 }
         }
-        program[MAX_LEN] = '\0';
+        program[MAX_PROGRAM_LEN] = '\0';
         struct ReadReturn result = validate(program, strlen(program));
         if (result.type == ERROR) {
                 return 0;
