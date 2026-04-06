56ba689a9830f31d5a2341d4ef90f75d3223c8d3
benmandrew <benmandrew@gmail.com>
2026-04-06 02:55:02 +0100

    chore(web): Move go server to web directory
    Can delete the `go` directory to clean up the project root.

--- web/server.go
+++ web/server.go
@@ -11,11 +11,11 @@ import (
 )
 
 var cache map[string][]byte
-var n_requests int64
-var n_cache_hits int64
-var compilation_duration_sum_seconds float64
+var nRequests int64
+var nCacheHits int64
+var compilationDurationSumSeconds float64
 
-var allowed_chars = map[rune]bool{
+var allowedChars = map[rune]bool{
 	'>':  true,
 	'<':  true,
 	'+':  true,
@@ -33,7 +33,7 @@ type bfError struct {
 }
 
 func (e *bfError) Error() string {
-	return fmt.Sprintf("%s", e.message)
+	return e.message
 }
 
 func sanitiseInput(input string) (string, error) {
@@ -41,22 +41,24 @@ func sanitiseInput(input string) (string, error) {
 	if len(input) > 10000 {
 		return "", &bfError{message: "Input too long"}
 	}
-	loop_stack_depth := 0
+
+	loopStackDepth := 0
 	for i, char := range input {
-		if !allowed_chars[char] {
+		if !allowedChars[char] {
 			return "", &bfError{
 				message: fmt.Sprintf("Invalid character %q at location %d", char, i+1),
 			}
 		}
 		filteredInput.WriteRune(char)
-		switch {
-		case char == '[':
-			loop_stack_depth++
-		case char == ']':
-			loop_stack_depth--
+		switch char {
+		case '[':
+			loopStackDepth++
+		case ']':
+			loopStackDepth--
 		}
 	}
-	if loop_stack_depth != 0 {
+
+	if loopStackDepth != 0 {
 		return "", &bfError{message: "Mismatched loops"}
 	}
 	return filteredInput.String(), nil
@@ -74,52 +76,59 @@ func runHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "GET required", http.StatusMethodNotAllowed)
 		return
 	}
+
 	input := []byte(r.URL.Query().Get("code"))
-	str_input, err := sanitiseInput(string(input))
+	strInput, err := sanitiseInput(string(input))
 	if err != nil {
 		http.Error(w, "Invalid input: "+err.Error(), http.StatusBadRequest)
 		return
 	}
-	n_requests++
+
+	nRequests++
 	w.Header().Set("Content-Type", "text/plain")
-	if output, found := cache[str_input]; found {
-		n_cache_hits++
-		log.Printf("Cache hit, total requests: %d, cache hits: %d", n_requests, n_cache_hits+1)
+	if output, found := cache[strInput]; found {
+		nCacheHits++
+		log.Printf("Cache hit, total requests: %d, cache hits: %d", nRequests, nCacheHits)
 		w.Write(output)
 		return
 	}
+
 	programPath := os.Args[1]
 	cmd := exec.Command(programPath)
-	cmd.Stdin = bytes.NewReader([]byte(str_input))
+	cmd.Stdin = bytes.NewReader([]byte(strInput))
+
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
+
 	start := time.Now()
 	err = cmd.Run()
-	compilation_duration_sum_seconds += time.Since(start).Seconds()
+	compilationDurationSumSeconds += time.Since(start).Seconds()
 	if err != nil {
 		log.Printf("Error: %v, stderr: %s", err, stderr.String())
 		http.Error(w, stderr.String(), http.StatusBadRequest)
 		return
 	}
+
 	w.Write(stdout.Bytes())
-	cache[string(input)] = stdout.Bytes()
+	cache[strInput] = stdout.Bytes()
 }
 
 func metricsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
-	compilations := n_requests - n_cache_hits
+	compilations := nRequests - nCacheHits
 	meanCompilationDuration := 0.0
 	if compilations > 0 {
-		meanCompilationDuration = compilation_duration_sum_seconds / float64(compilations)
+		meanCompilationDuration = compilationDurationSumSeconds / float64(compilations)
 	}
+
 	fmt.Fprintf(w, "# HELP bfc_requests_total Total number of bfc execution requests\n")
 	fmt.Fprintf(w, "# TYPE bfc_requests_total counter\n")
-	fmt.Fprintf(w, "bfc_requests_total %d\n", n_requests)
+	fmt.Fprintf(w, "bfc_requests_total %d\n", nRequests)
 	fmt.Fprintf(w, "# HELP bfc_cache_hits_total Total number of bfc cache hits\n")
 	fmt.Fprintf(w, "# TYPE bfc_cache_hits_total counter\n")
-	fmt.Fprintf(w, "bfc_cache_hits_total %d\n", n_cache_hits)
+	fmt.Fprintf(w, "bfc_cache_hits_total %d\n", nCacheHits)
 	fmt.Fprintf(w, "# HELP bfc_compilations_total Total number of bfc compilations (cache misses)\n")
 	fmt.Fprintf(w, "# TYPE bfc_compilations_total counter\n")
 	fmt.Fprintf(w, "bfc_compilations_total %d\n", compilations)
@@ -133,9 +142,10 @@ func metricsHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	cache = make(map[string][]byte)
-	n_requests = 0
-	n_cache_hits = 0
-	compilation_duration_sum_seconds = 0
+	nRequests = 0
+	nCacheHits = 0
+	compilationDurationSumSeconds = 0
+
 	http.HandleFunc("/", runHandler)
 	http.HandleFunc("/metrics", metricsHandler)
 	log.Println("Listening on :8000 at /")
