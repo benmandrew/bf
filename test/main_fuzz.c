68c0c67f1fd935fe0c163a6215f48fab0b9fc2c5
benmandrew <benmandrew@gmail.com>
2026-06-30 21:47:18 +0100

    fuzz: enable LLVM passes and add clear_loop seed
    Two uncovered paths in the harness:
    - generate() was called with optimise=false, leaving the entire LLVM
      pass pipeline (mem2reg, instcombine, simplifycfg, gvn) unreachable
    - No seed triggered detect_clear_loops(), so CMD_CLEAR and the clear()
      codegen function were dead code; add [-] to the corpus to fix that

--- test/main_fuzz.c
+++ test/main_fuzz.c
@@ -35,7 +35,7 @@ int main(int argc, char **argv) {
                         continue;
                 struct program p = string_to_program(r.value.program_str);
                 optimise_program(&p);
-                LLVMModuleRef module = generate(&p, false);
+                LLVMModuleRef module = generate(&p, true);
                 char *module_str = LLVMPrintModuleToString(module);
                 LLVMDisposeMessage(module_str);
                 dispose_module(module);
